4e9566fb2783c0219caaf44bc2da3d47f36949d3
Andreas Hoeppener <127937567+hoeppener@users.noreply.github.com>
2023-07-04 10:58:39 +0200

    Update and rename createUser.zsh to setupAdmin.zsh

--- setupAdmin.zsh
+++ setupAdmin.zsh
@@ -1,15 +1,17 @@
 #!/bin/zsh
 
+# determine next available UID
+highestUID=$( dscl . -list /Users UniqueID | /usr/bin/awk '$2>m {m=$2} END { print m }' )
+nextUID=$(( highestUID+1 ))
+
+echo "UID reserved: $nextUID"
+
 # new user account details
-username="testAdmin"
+username=admin
 displayName="Pacemaker Admin"
 password="P@55w0rd"
 admin="yes"
 hidden="yes"
-
-# determine next available UID
-highestUID=$( dscl . -list /Users UniqueID | /usr/bin/awk '$2>m {m=$2} END { print m }' )
-nextUID=$(( highestUID+1 ))
 
 # create the account
 /usr/bin/dscl . create "/Users/$username"
@@ -19,15 +21,22 @@ nextUID=$(( highestUID+1 ))
 /usr/bin/dscl . create "/Users/$username" PrimaryGroupID 20
 /usr/bin/dscl . passwd "/Users/$username" "$password"
 
+echo "Account structure created"
+
 # make the account admin, if specified
 if [[ "$admin" = "yes" ]]; then
     /usr/bin/dscl . append /Groups/admin GroupMembership "$username"
+	echo "added to admin group"
 fi
 
 # hide the account, if specified
 if [[ "$hidden" = "yes" ]]; then
     /usr/bin/dscl . create "/Users/$username" IsHidden 1
     /usr/bin/dscl . create "/Users/$username" NFSHomeDirectory "/private/var/$username"
+echo "added to admin group" Account hidden
+
 else
     /usr/bin/dscl . create "/Users/$username" NFSHomeDirectory "/Users/$username"
 fi
+
+echo "Done!"
